cc6e48e4d0e61fee641c250b304933fdcbe0eec0
Tao Li <litao@sogou-inc.com>
2016-06-06 20:57:34 +0800

    Implement download result.

--- src/main/java/com/sogou/beaver/resources/JobResources.java
+++ src/main/java/com/sogou/beaver/resources/JobResources.java
@@ -1,15 +1,19 @@
 package com.sogou.beaver.resources;
 
-import com.sogou.beaver.core.plan.ParseException;
-import com.sogou.beaver.dao.JobDao;
-import com.sogou.beaver.db.ConnectionPoolException;
 import com.sogou.beaver.core.plan.ExecutionPlan;
+import com.sogou.beaver.core.plan.ParseException;
 import com.sogou.beaver.core.plan.QueryPlan;
 import com.sogou.beaver.core.plan.QueryPlanParser;
+import com.sogou.beaver.dao.JobDao;
+import com.sogou.beaver.db.ConnectionPoolException;
 import com.sogou.beaver.model.Job;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
@@ -41,5 +45,14 @@ public class JobResources {
                            @QueryParam("page") long page, @QueryParam("size") long size)
       throws ConnectionPoolException, SQLException {
     return dao.getJobsByUserId(userId, page, size);
+  }
+
+  @GET
+  @Path("/download")
+  @Produces(MediaType.APPLICATION_OCTET_STREAM)
+  public Response download(@QueryParam("id") long id) throws FileNotFoundException {
+    return Response.ok(new FileInputStream("data/" + id + ".data"))
+        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + id + ".csv")
+        .build();
   }
 }
