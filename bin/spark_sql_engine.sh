52e42e8580dd8713fffbdeb6820a8056a184a804
Tao Li <litao@sogou-inc.com>
2016-06-18 17:56:21 +0800

    Add spark-sql-collector.

--- bin/spark_sql_engine.sh
+++ bin/spark_sql_engine.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-sql=`cat -`
-echo "sql: $sql"
+dir=`dirname $0`
+dir=`cd $dir/..; pwd`
+
+if [ $# -ne 1 ]; then
+  echo "usage: $0 <jobId>"
+  exit 1
+fi
+
+jobId=$1
+hdfsOutput=beaver/output/$jobId
+
+hadoop fs -rmr $hdfsOutput
+hadoop fs -mkdir -p $hdfsOutput
+
+cat - | $dir/bin/spark-sql-collector $hdfsOutput
+
+if [ $? -ne 0 ]; then
+  echo "failed to run the job: $jobId"
+  exit 1
+fi
+
+localOutput=$dir/data/$jobId.data
+rm -f $localOutput
+hadoop fs -getmerge $hdfsOutput/part-* $localOutput
--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-mvn package
-mkdir -p logs/jobs
-java -jar target/beaver-service-1.0-SNAPSHOT.jar server conf/beaver.yml
+dir=`dirname $0`
+dir=`cd $dir/..; pwd`
+
+cd $dir; mvn package
+
+mkdir -p $dir/logs/jobs
+java -jar $dir/target/beaver-service-1.0-SNAPSHOT.jar server $dir/conf/beaver.yml
