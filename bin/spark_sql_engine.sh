778ef52996497d6b2e277c4a7cf4f08081c57a90
Tao Li <litao@sogou-inc.com>
2016-06-18 18:06:28 +0800

    Update spark_sql_engine.sh

--- bin/spark_sql_engine.sh
+++ bin/spark_sql_engine.sh
@@ -9,18 +9,23 @@ if [ $# -ne 1 ]; then
 fi
 
 jobId=$1
-hdfsOutput=beaver/output/$jobId
+hdfsRoot=beaver/output
+hdfsOutput=$hdfsRoot/$jobId
+localOutput=$dir/data/$jobId.data
 
-hadoop fs -rmr $hdfsOutput
-hadoop fs -mkdir -p $hdfsOutput
+hadoop fs -mkdir -p $hdfsRoot
+hadoop fs -rm -r $hdfsOutput
 
-cat - | $dir/bin/spark-sql-collector $hdfsOutput
+# TODO support dynamic pass the spark config
+cat - | spark-submit \
+  --master "local[*]" \
+  --class com.sogou.spark.sql.SparkSQLCollector \
+  $dir/bin/spark-sql-collector $hdfsOutput
 
 if [ $? -ne 0 ]; then
   echo "failed to run the job: $jobId"
   exit 1
 fi
 
-localOutput=$dir/data/$jobId.data
 rm -f $localOutput
 hadoop fs -getmerge $hdfsOutput/part-* $localOutput
