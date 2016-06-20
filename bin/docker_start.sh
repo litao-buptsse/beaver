bdf432291b1ace362ad66b19e2201f9add4a077f
litao <litao@sogou-inc.com>
2016-06-21 00:18:04 +0800

    Update docker_start.sh

--- bin/docker_start.sh
+++ bin/docker_start.sh
@@ -4,13 +4,11 @@ REGISTRY=registry.docker.dev.sogou-inc.com:5000
 IMAGE=beaver/beaver-service
 VERSION=1.0-SNAPSHOT
 
-PORT=9080
-ADMIN_PORT=9081
 LOG_DIR=/search/ted/beaver/logs
 DATA_DIR=/search/ted/beaver/data
 
-docker run -d \
-  -p $PORT:8080 -p $ADMIN_PORT:8081 \
+docker run -d --net=host \
+  -v /etc/localtime:/etc/localtime \
   -v $LOG_DIR:/search/beaver/logs \
   -v $LOG_DIR:/search/beaver/data \
   $REGISTRY/$IMAGE:$VERSION
